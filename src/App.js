b9c4e8c611f24b6ae5eaaa413c9673fbe7ee3e75
Christian Leyva <chrisley304@hotmail.com>
2022-12-03 20:55:51 -0600

    La regue al poner la ruta de reglas de asociacion

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Sidebar} from "./components/Sidebar/Sidebar"
 import {Dashboard} from "./pages/Dashboard/Dashboard"
 import {PageNotFound} from "./pages/PageNotFound/PageNotFound"
+import {ReglasAsociacion} from "./pages/ReglasAsociacion/ReglasAsociacion"
 
 function App() {
     return (
@@ -10,7 +11,7 @@ function App() {
             <Sidebar />
             <Routes>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="/reglas-asociacion" element={<Dashboard />} />
+                <Route path="/reglas-asociacion" element={<ReglasAsociacion />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
