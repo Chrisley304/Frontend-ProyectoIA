07e93e525013b2b8329831956d159f533941fea9
Christian Leyva <chrisley304@hotmail.com>
2022-12-03 23:25:56 -0600

    Añadi el github repo al main

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Grid } from "@nextui-org/react";
+import { Grid, Card, Link, Text } from "@nextui-org/react";
 import {Page} from "../../components/Page/Page";
 import { CardInicio } from "../../components/CardInicio/CardInicio";
 import imgCardAsociacion from "../../assets/img/card-1.jpg";
 import imgCardDistancia from "../../assets/img/card-distancia.jpg"
 import imgCardClustering from "../../assets/img/card-clustering.jpg";
+import githubLogo from "../../assets/img/GitHub-Mark-120px-plus.png";
 
 export const Dashboard = ()=>{
 
@@ -45,13 +46,47 @@ export const Dashboard = ()=>{
                         linkPagina="/clustering"
                     />
                 </Grid>
-                {/* <Grid xs={12} sm={5}>
-                    <CardInicio
-                        titulo="Prueba las reglas de asociación!"
-                        categoria="Algoritmo"
-                        imagen="src/assets/img/penguin.png"
-                    />
-                </Grid> */}
+                <Grid xs={12}>
+                    <Card css={{ p: "$6" }}>
+                        <Card.Header>
+                            <img
+                                alt="github logo"
+                                src={githubLogo}
+                                width="34px"
+                                height="34px"
+                            />
+                            <Grid.Container css={{ pl: "$6" }}>
+                                <Grid xs={12}>
+                                    <Text h4 css={{ lineHeight: "$xs" }}>
+                                        Revisa el codigo fuente del proyecto!
+                                    </Text>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <Text css={{ color: "$accents8" }}>
+                                        github.com/Chrisley304/Frontend-ProyectoIA
+                                    </Text>
+                                </Grid>
+                            </Grid.Container>
+                        </Card.Header>
+                        <Card.Body css={{ py: "$2" }}>
+                            <Text>
+                                El codigo fuente es <strong>open source</strong>{" "}
+                                por lo que puedes revisarlo si así lo deseas en
+                                mi perfil de Github!
+                            </Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <Link
+                                icon
+                                color="primary"
+                                target="_blank"
+                                href="https://github.com/Chrisley304/Frontend-ProyectoIA"
+                            >
+                                Visita el código fuente en GitHub.
+                            </Link>
+                        </Card.Footer>
+                    </Card>
+                </Grid>
                 {/* <Grid xs={12} sm={7}>
                     <CardInicio
                         titulo="Prueba las reglas de asociación!"
