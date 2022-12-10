25c9448df236d0ffdfc5f3e15a9c95c6977da6a4
Christian Leyva <chrisley304@hotmail.com>
2022-12-09 21:58:19 -0600

    Termine sección de metricas de distancia

--- src/pages/MetricasDistancia/MetricasDistancia.js
+++ src/pages/MetricasDistancia/MetricasDistancia.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Page } from "../../components/Page/Page";
-import { Input, Button, Grid, Card, Text, Progress } from "@nextui-org/react";
+import { Input, Button, Grid, Card, Text, Progress, Radio } from "@nextui-org/react";
 import "./MetricasDistancia.css";
 import { useRef } from "react";
 import Papa from "papaparse";
@@ -19,15 +19,16 @@ const API = process.env.REACT_APP_API_URL;
 export const MetricasDistancia = () => {
     // Para el label del file
     const [filenameLabel, setFilenameLabel] = useState("");
-    const [soporteMinimo, setSoporteMinimo] = useState();
-    const [confianzaMinima, setConfianzaMinima] = useState();
-    const [elevacionMinima, setElevacionMinima] = useState();
-    const [respuestaNReglas, setRespuestaNReglas] = useState(-1);
+    const [metricaSeleccionada, setMetricaSeleccionada] = useState();
     const [errorRespuesta, setErrorRespuesta] = useState(false);
     const [textoError, setTextoError] = useState("");
     const [dataTable, setDataTable] = useState();
     const [headerTable, setHeaderTable] = useState();
     const [isLoading, setIsLoading] = useState(false);
+    const [salida, setSalida] = useState(false);
+    const [objetoA, setObjetoA] = useState();
+    const [objetoB, setObjetoB] = useState();
+    const [resultadoDistancia, setResultadoDistancia] = useState();
 
     // Reference for the invisible file input, to modify the beauty one
     const inputFile = useRef(null);
@@ -62,25 +63,26 @@ export const MetricasDistancia = () => {
     }, [filenameLabel]);
 
     const isFormValid =
-        soporteMinimo &&
-        confianzaMinima &&
-        elevacionMinima &&
+        metricaSeleccionada &&
         validateFileExt(filenameLabel);
+    const isFormValidForm2 =
+        metricaSeleccionada && validateFileExt(filenameLabel) && objetoA && objetoB && objetoA !== objetoB;
 
-    const handleSubmit = async (e) => {
+    const handleSubmitMatriz = async () => {
         setIsLoading(true);
-        e.preventDefault();
+        
         const formData = new FormData(form.current);
-        const res = await fetch(API + "asociacion", {
-            method: "POST",
-            body: formData,
-        });
+        const res = await fetch(
+            API + "/matriz-distancia/" + metricaSeleccionada,
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
         const infoRes = await res.json();
         // console.log(infoRes);
         if (!('error' in infoRes)){
             const csvFile = infoRes["csv"];
-            setRespuestaNReglas(infoRes["nReglas"]);
-
             const parsedCsv = Papa.parse(csvFile, { header: true });
             const parsedData = parsedCsv?.data;
             var tableHeaders = [];
@@ -95,6 +97,30 @@ export const MetricasDistancia = () => {
             // console.log(parsedData);
             setDataTable(parsedData);
             setHeaderTable(tableHeaders);
+            setSalida(true);
+        }else{
+            setTextoError(infoRes['error']);
+            setErrorRespuesta(true);
+            setSalida(false);
+        }
+        setIsLoading(false);
+    };
+
+    const handleSubmitDistObj = async () => {
+        setIsLoading(true);
+        
+        const formData = new FormData(form.current);
+        const res = await fetch(
+            API + "/distancia-objetos/" + metricaSeleccionada,
+            {
+                method: "POST",
+                body: formData,
+            }
+        );
+        const infoRes = await res.json();
+        // console.log(infoRes);
+        if (!('error' in infoRes)){
+            setResultadoDistancia(infoRes["distancia"]);
         }else{
             setTextoError(infoRes['error']);
             setErrorRespuesta(true);
@@ -104,95 +130,165 @@ export const MetricasDistancia = () => {
 
     return (
         <Page
-            titulo="Metricas de distancia"
+            titulo="Métricas de distancia"
             descripcion="En esta sección de la app puedes obtener las metricas de distancia de un dataset que ingreses en CSV, utilizando el algoritmo que desees."
         >
             <Grid.Container gap={2}>
-                <Grid xs={12} sm={4}>
-                    <form ref={form} onSubmit={handleSubmit}>
-                        <Grid.Container gap={2}>
-                            <Grid xs={12}>
-                                <input
-                                    ref={inputFile}
-                                    type="file"
-                                    name="file"
-                                    style={{ display: "none" }}
-                                    onChange={handleFileInput}
-                                ></input>
-                                <Input
-                                    readOnly
-                                    labelLeft="Archivo"
-                                    className="boton-archivo"
-                                    initialValue={filenameLabel}
-                                    helperColor={fileHelper.color}
-                                    status={fileHelper.color}
-                                    helperText={fileHelper.text}
-                                    label="Ingresa un archivo .csv a analizar"
-                                    onClick={selectFile}
-                                />
+                <Grid.Container>
+                    <Grid xs={12} sm={8}>
+                        <form
+                            ref={form}
+                            // onSubmit={handleSubmit}
+                            className="form-container"
+                        >
+                            <Grid xs={6}>
+                                <Grid.Container gap={2}>
+                                    <Grid xs={12}>
+                                        <input
+                                            ref={inputFile}
+                                            type="file"
+                                            name="file"
+                                            style={{ display: "none" }}
+                                            onChange={handleFileInput}
+                                        ></input>
+                                        <Input
+                                            readOnly
+                                            labelLeft="Archivo"
+                                            className="boton-archivo"
+                                            initialValue={filenameLabel}
+                                            helperColor={fileHelper.color}
+                                            status={fileHelper.color}
+                                            helperText={fileHelper.text}
+                                            label="Ingresa un archivo .csv a analizar"
+                                            onClick={selectFile}
+                                        />
+                                    </Grid>
+                                    <Grid xs={12}>
+                                        <Radio.Group
+                                            label="Métrica a utilizar"
+                                            value={metricaSeleccionada}
+                                            onChange={setMetricaSeleccionada}
+                                        >
+                                            <Radio value="euclidean">
+                                                Métrica Euclidiana
+                                            </Radio>
+                                            <Radio value="chebyshev">
+                                                Métrica Chebyshev
+                                            </Radio>
+                                            <Radio value="cityblock">
+                                                Métrica de Manhattan (City
+                                                block)
+                                            </Radio>
+                                        </Radio.Group>
+                                    </Grid>
+                                    <Grid xs={12}>
+                                        <Button
+                                            flat
+                                            size={"lg"}
+                                            color="primary"
+                                            disabled={!isFormValid}
+                                            type="button"
+                                            onPress={handleSubmitMatriz}
+                                            value="matriz-distancias"
+                                        >
+                                            Obtener metricas de distancia
+                                        </Button>
+                                    </Grid>
+                                </Grid.Container>
                             </Grid>
-                            <Grid xs={12}>
-                                <Input
-                                    helperText=""
-                                    type="number"
-                                    step={0.001}
-                                    name="soporteMinimo"
-                                    label="Soporte mínimo"
-                                    onChange={(e) =>
-                                        setSoporteMinimo(e.target.value)
-                                    }
-                                    placeholder="Ej. 0.01"
-                                />
+                            <Grid xs={6}>
+                                <Grid.Container gap={2}>
+                                    <Grid xs={12}>
+                                        <Input
+                                            helperText=""
+                                            type="number"
+                                            step={1}
+                                            min={0}
+                                            name="objeto1"
+                                            label="Objeto A:"
+                                            onChange={(e) =>
+                                                setObjetoA(e.target.value)
+                                            }
+                                            placeholder="Ej. 1"
+                                        />
+                                    </Grid>
+                                    <Grid xs={12}>
+                                        <Input
+                                            helperText=""
+                                            type="number"
+                                            step={1}
+                                            min={0}
+                                            name="objeto2"
+                                            label="Objeto B:"
+                                            onChange={(e) =>
+                                                setObjetoB(e.target.value)
+                                            }
+                                            placeholder="Ej. 2"
+                                        />
+                                    </Grid>
+
+                                    <Grid xs={12}>
+                                        <Button
+                                            flat
+                                            size={"lg"}
+                                            color="secondary"
+                                            disabled={!isFormValidForm2}
+                                            type="button"
+                                            onPress={handleSubmitDistObj}
+                                            value="distancia-objetos"
+                                        >
+                                            Obtener distancia entre elementos
+                                        </Button>
+                                    </Grid>
+                                </Grid.Container>
                             </Grid>
-                            <Grid xs={12}>
-                                <Input
-                                    helperText=""
-                                    type="number"
-                                    step={0.001}
-                                    name="confianzaMinima"
-                                    onChange={(e) =>
-                                        setConfianzaMinima(e.target.value)
-                                    }
-                                    label="Confianza mínima"
-                                    placeholder="Ej. 0.3"
-                                />
-                            </Grid>
-                            <Grid xs={12}>
-                                <Input
-                                    helperText=""
-                                    type="number"
-                                    step={0.001}
-                                    name="elevacionMinima"
-                                    onChange={(e) =>
-                                        setElevacionMinima(e.target.value)
-                                    }
-                                    label="Elevación mínima"
-                                    placeholder="Ej. 2"
-                                />
-                            </Grid>
-                            <Grid xs={12}>
-                                <Button
-                                    flat
-                                    size={"lg"}
-                                    color="primary"
-                                    disabled={!isFormValid}
-                                    type="submit"
-                                    value="Submit"
-                                >
-                                    Obtener reglas de asociacion
-                                </Button>
-                            </Grid>
-                        </Grid.Container>
-                    </form>
-                </Grid>
+                        </form>
+                    </Grid>
+                    <Grid xs={12} sm={4}>
+                        <div className="resultados-container">
+                            <Card
+                                className="card-resultados"
+                                css={{
+                                    maxHeight: "700px",
+                                    h: "100%",
+                                    overflow: "scroll",
+                                }}
+                            >
+                                <Card.Body className="card-resultados-body">
+                                    <Text h3>
+                                        Distancia {metricaSeleccionada} entre
+                                        los objetos {objetoA} & {objetoB}:
+                                    </Text>
+                                    {resultadoDistancia ? (
+                                        <div className="waiting-container">
+                                            <Text size={20}>{resultadoDistancia}</Text>
+                                        </div>
+                                    ) : (
+                                        <div className="waiting-container">
+                                            <Text css={{ pb: "$10" }}>
+                                                Esperando entrada...
+                                            </Text>
+                                            <Progress
+                                                indeterminated
+                                                value={50}
+                                                color="secondary"
+                                                status="secondary"
+                                            />
+                                        </div>
+                                    )}
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Grid>
+                </Grid.Container>
                 <LoadingModal visible={isLoading} />
                 {/* Si hay un error, se muestra el modal de error */}
                 {errorRespuesta && <ModalError textoError={textoError} />}
                 {/* Si no se generaron reglas, se muestra el error */}
-                {respuestaNReglas === 0 && (
+                {/* {respuestaNReglas === 0 && (
                     <ModalError textoError="La configuración ingresada no genero ninguna regla de asociación. Actualiza los valores e intenta de nuevo." />
-                )}
-                <Grid xs={12} sm={8}>
+                )} */}
+                <Grid xs={12}>
                     <div className="resultados-container">
                         <Card
                             className="card-resultados"
@@ -203,8 +299,22 @@ export const MetricasDistancia = () => {
                             }}
                         >
                             <Card.Body className="card-resultados-body">
-                                <Text h3>Frecuencia de los elementos:</Text>
-                                {respuestaNReglas <= 0 ? (
+                                <Text h3>
+                                    Matriz de distancia {metricaSeleccionada} de
+                                    los elementos:
+                                </Text>
+                                {salida ? (
+                                    <div>
+                                        {/* <Text>
+                                            Se generaron{" "}
+                                            <b>{respuestaNReglas}</b> reglas:
+                                        </Text> */}
+                                        <TablaAsociacion
+                                            data={dataTable}
+                                            cols={headerTable}
+                                        />
+                                    </div>
+                                ) : (
                                     <div className="waiting-container">
                                         <Text css={{ pb: "$10" }}>
                                             Esperando entrada...
@@ -214,17 +324,6 @@ export const MetricasDistancia = () => {
                                             value={50}
                                             color="secondary"
                                             status="secondary"
-                                        />
-                                    </div>
-                                ) : (
-                                    <div>
-                                        <Text>
-                                            Se generaron{" "}
-                                            <b>{respuestaNReglas}</b> reglas:
-                                        </Text>
-                                        <TablaAsociacion
-                                            data={dataTable}
-                                            cols={headerTable}
                                         />
                                     </div>
                                 )}
